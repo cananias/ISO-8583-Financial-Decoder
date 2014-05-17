82b091f04c6cdf354bc2138d818c37553c221512
Carlos Ananías <cananias@encler.com>
2014-05-17 00:51:49 -0400

    Version 0.1.1
    Incluye índex.html

---
+++ pruebas.js
@@ -0,0 +1,9 @@
+var mapa =  {};
+
+//mapa['BIT_1'] = 'Extensión del Bitmap;16';
+//mapa['BIT_2'] = 'Extensión del Bitmap;16';
+//mapa['BIT_3'] = 'Extensión del Bitmap;16';
+debug(Object.keys(mapa))
+if(Object.keys(mapa) == ""){
+  debug("a")
+}
