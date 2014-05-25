089a8b63eb2a059ebee15ae04ca48377b6a379f7
Carlos Ananías <cananias@encler.com>
2014-05-25 01:00:28 -0400

    0.1.3
    Se define como será la “Vista” de la primera versión.

--- js/app/implementDecoder.js
+++ js/app/implementDecoder.js
@@ -4,13 +4,19 @@ function decode(){
   if( message != ""){
 
     var iso = new Iso8583(message);
-    document.getElementById('isoLength').value = iso.msgLength();
-    document.getElementById('messageType').value = iso.msgType();  
+
+    document.getElementById('msgLength').innerHTML = '<b>Message Length: </b>'+iso.msgLength();
+    document.getElementById('mti').innerHTML = '<b>Message Type: </b>'+iso.msgType();
+    document.getElementById('bitmap1').innerHTML = '<b>Bitmap 1: </b>'+iso.msgBitmap1();
+    document.getElementById('bitmap2').innerHTML = '<b>Bitmap 2: </b>'+iso.msgBitmap2();
+    document.getElementById('rowDecodedContent').innerHTML = '<tr><td>2</td><td>Yes</td><td>Primary account number (PAN)</td><td>'+iso.msgDecoded.bit2+'</td></tr>';
+
+
   
   }
   else{
 
-    alert("Please, Insert a ISO Message...")   
+    alert('Not found message to decode. Please insert them.')
   
   }
 }
