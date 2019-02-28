b751bb86d81346cd4d1806a313c68ef87e99dc83
Encler <cananias@encler.com>
2019-02-28 17:44:24 -0300

    0.2.4
    Se mejora proceso para decodificación de Bits.

--- js/app/implementDecoder.js
+++ js/app/implementDecoder.js
@@ -3,6 +3,21 @@ function decode(){
 
   if( message != ""){
 
+
+    var len_str = message.length-4;
+    console.log("len_str: "+len_str);
+
+    var len_iso = message.substring(0,4)*1;
+    console.log("len_iso: "+len_iso);
+
+    if (len_str != len_iso) {
+      alert_msj = '<div class="alert alert-warning alert-dismissible masterAlert masterDangerAlert" role="alert"><button type="button" class="close" data-dismiss="alert" aria-label="Close"><span aria-hidden="true">×</span></button><p>Largo del mensaje agregado automaticamente: <strong> '+("0000"+message.length).slice(-4)+' </strong>.</p></div>';
+      document.getElementById('alert-place').innerHTML = alert_msj;
+      message = ("0000"+message.length).slice(-4)+message;
+      console.log("Largo del mensaje agregado automaticamente: "+("0000"+message.length).slice(-4));
+    }
+
+
     var iso = new Iso8583(message);
     var isoDecoded = "";
     var bitValue = "";
